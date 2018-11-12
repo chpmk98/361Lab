6316cab2f1f88aca564a1b1511c66f8022361894
mhjasonliu <jasonliu2020.1@u.northwestern.edu>
2018-11-12 10:33:36 -0600

    alu package

---
+++ ALU/ALUPackage.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.eecs361_gates.all;
+use work.eecs361.all;
+
+package ALUPackage is
+    component final_alu_32_v2 is
+	    port(
+	    a	: in std_logic_vector(31 downto 0);
+	    b	: in std_logic_vector(31 downto 0);
+	    ctrl: in std_logic_vector(3 downto 0);
+	    s 	: out std_logic_vector(31 downto 0);
+	    z   : out std_logic;
+	    cout: out std_logic;
+	    ovflow: out std_logic
+	    );
+	end component final_alu_32_v2;
+end;
